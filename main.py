848ba86d7a758961ed9b3f6b8b8086f9d354a8e6
Abdullah <heighter79@gmail.com>
2023-08-23 15:29:39 -0700

    Finally Made it...!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle, pandas
+
 data = pandas.read_csv("50_states.csv")
 
 screen = turtle.Screen()
@@ -6,10 +7,23 @@ screen.title("Data-Analysis-US-States-Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-
+states = []
+counter = 0
 
 for answer in range(50):
-    answer_in = screen.textinput(title="Guess the State", prompt="What's the next state").lower()
-    print(answer_in)
+    answer_in = screen.textinput(title=f"{counter + 1 } / 50 Guess the State", prompt="What's the next state").title()
+    if answer_in in data["state"].tolist():
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_d = data[data.state == answer_in]
+        t.goto(int(state_d.x), int(state_d.y))
+        t.write(state_d.state)
+        states.append(answer_in)
+        counter += 1
+        print(data[data.state == answer_in])
 
-turtle.mainloop()
+    else:
+        print("irrelevant entry")
+
+screen.exitonclick()
